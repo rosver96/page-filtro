bb1b34e7b057482df0574c7cad25c1fab6918956
tatiana <tatianacaldeorn10@gmail.com>
2023-06-16 20:28:28 -0500

    se hacen cambios

--- cars.js
+++ cars.js
@@ -1,13 +1,10 @@
-// funcion de buscador llamada 
-
-const buscador = document.getElementById('buscador')
-buscador.addEventListener('input', e => {
+const buscador = document.getElementById('buscador');
+buscador.addEventListener('input', (e) => {
   const textoBuscado = e.target.value;
 
-
-  document.querySelectorAll('#idhijo').forEach(fila => {
-      const primeraCelda =  fila.querySelector('#ididentidad');
-      console.log(primeraCelda);
+  document.querySelectorAll('#idhijo').forEach((fila) => {
+    const primeraCelda = fila.querySelector('#ididentidad');
+    console.log(primeraCelda);
     const textoCelda = primeraCelda.textContent;
 
     if (textoCelda.includes(textoBuscado)) {
@@ -18,10 +15,9 @@ buscador.addEventListener('input', e => {
   });
 });
 
-// gestion de clientes 
+// gestion de clientes
 const formularioGestionClientes = document.getElementById('formularioGestionClientes');
 const padreTabla = document.getElementById('TablaGestionClientes');
-
 
 // informacion de los id de los input //
 const identificacion = document.getElementById('identificacion');
@@ -32,129 +28,129 @@ const tipo = document.getElementById('tipo');
 const correo = document.getElementById('correo');
 const telefono = document.getElementById('telefono');
 
-
 // array//
 const gestionClientes = [];
 
 // funcion listar clientes //
-function listarClientes(gestionClientes){
-    padreTabla.innerHTML = '';
-    gestionClientes.forEach((e, index) => {
-        let hijo = document.createElement('tr');
-        hijo.classList.add('table-secondary','tabla');
-        hijo.id = 'idhijo';  
-        
-        let clienteid = document.createElement('td');
-        clienteid.textContent = `${e,idC}`;
-        hijo.appendChild(clienteid) ;
-        clienteid.id = 'identidad'; 
+function listarClientes(gestionClientes) {
+  padreTabla.innerHTML = '';
+  gestionClientes.forEach((e, index) => {
+    let hijo = document.createElement('tr');
+    hijo.classList.add('table-secondary', 'tabla');
+    hijo.id = 'idhijo';
 
-        let clientenombre = document.createElement('td');
-        clientenombre.textContent = `${nombreC}`;
-        hijo.appendChild(clientenombre);
+    let clienteid = document.createElement('td');
+    clienteid.textContent = `${e.idC}`;
+    hijo.appendChild(clienteid);
+    clienteid.id = 'identidad';
 
-        let clienteapellido = document.createElement('td');
-        clienteapellido.textContent = `${e,apellidoC}`;
-        hijo.appendChild(clienteapellido);
+    let clientenombre = document.createElement('td');
+    clientenombre.textContent = `${e.nombreC}`;
+    hijo.appendChild(clientenombre);
 
-        let clienteplaca = document.createElement('td');
-        clienteplaca.textContent = `${e,placaC}`;
-        hijo.appendChild(clienteplaca);
+    let clienteapellido = document.createElement('td');
+    clienteapellido.textContent = `${e.apellidoC}`;
+    hijo.appendChild(clienteapellido);
 
-        let clientetipo = document.createElement('td');
-        clientetipo.textContent = `${e,tipoC}`;
-        hijo.appendChild(clientetipo);
+    let clienteplaca = document.createElement('td');
+    clienteplaca.textContent = `${e.placaC}`;
+    hijo.appendChild(clienteplaca);
 
-        let clientecorreo = document.createElement('td');
-        clientecorreo.textContent = `${e,correoC}`;
-        hijo.appendChild(clientecorreo);
+    let clientetipo = document.createElement('td');
+    clientetipo.textContent = `${e.tipoC}`;
+    hijo.appendChild(clientetipo);
 
-        let clientetelefono = document.createElement('td');
-        clientetelefono.textContent = `${e,telefonoC}`;
-        hijo.appendChild(clientetelefono);
+    let clientecorreo = document.createElement('td');
+    clientecorreo.textContent = `${e.correoC}`;
+    hijo.appendChild(clientecorreo);
 
+    let clientetelefono = document.createElement('td');
+    clientetelefono.textContent = `${e.telefonoC}`;
+    hijo.appendChild(clientetelefono);
 
-        // boton eliminar y editar //
-        let botonEliminar = document.createElement('td');
-        eliminarBoton = document.createElement('button');
-        botonEditar = document.createElement('button');
-        botonEditar.classList.add('btn', 'btn-primary');
-        eliminarBoton.classList.add('btn', 'btn-danger');
-        eliminarBoton.textContent ='eliminar'
-        botonEditar.textContent ='editar';
-        botonEditar.dataset.index = index;
-        botonEliminar.appendChild(botonEditar);
-        botonEliminar.appendChild(eliminarBoton);
-        hijo.appendChild(botonEliminar);
+    // boton eliminar y editar //
+    let botonEliminar = document.createElement('td');
+    eliminarBoton = document.createElement('button');
+    botonEditar = document.createElement('button');
+    botonEditar.classList.add('btn', 'btn-primary');
+    eliminarBoton.classList.add('btn', 'btn-danger');
+    eliminarBoton.textContent = 'eliminar';
+    botonEditar.textContent = 'editar';
+    botonEditar.dataset.index = index;
+    botonEliminar.appendChild(botonEditar);
+    botonEliminar.appendChild(eliminarBoton);
+    hijo.appendChild(botonEliminar);
 
-        // se agrega el hijo al padre table//
-        padreTabla.appendChild(hijo);
+    // se agrega el hijo al padre table//
+    padreTabla.appendChild(hijo);
 
-        //llamdo al boton eliminar //
-        eliminarBoton.addEvenlistener('click', function(){
-            eliminarCliente(index);
-        });
-
-        botonEditar.addEvenlistener('click', function(){
-            editarCliente(index);
-        });
-        crearSelectorC();
+    //llamado al boton eliminar //
+    eliminarBoton.addEventListener('click', function () {
+      eliminarCliente(index);
     });
+
+    botonEditar.addEventListener('click', function () {
+      editarCliente(index);
+    });
+    crearSelectorC();
+  });
 }
 
 // funcion actualizar datos del cliente //
-function editarCliente(index){
-    const cliente = gestionClientes[index];
-    identificacion.value = cliente.idC;
-    nombre.value = cliente.nombreC;
-    apellido.value = cliente.apellidoC;
-    placa.value = cliente.placaC;
-    tipo.value = cliente.tipoC;
-    correo.value = cliente.correoC;
-    telefono.value = cliente.telefonoC;
+function editarCliente(index) {
+  const cliente = gestionClientes[index];
+  identificacion.value = cliente.idC;
+  nombre.value = cliente.nombreC;
+  apellido.value = cliente.apellidoC;
+  placa.value = cliente.placaC;
+  tipo.value = cliente.tipoC;
+  correo.value = cliente.correoC;
+  telefono.value = cliente.telefonoC;
 
-    formularioGestionClientes.addEventListener('submit', function (event) {
-        event.preventDefault(); 
-        cliente.idC = identificacion.value;
-        cliente.nombreC = nombre.value;
-        cliente.apellidoC = apellido.value;
-        cliente.placaC = placa.value;
-        cliente.tipoC = tipo.value;
-        cliente.correoC = correo.value;
-        cliente.telefonoC = telefono.value;
+  formularioGestionClientes.removeEventListener('submit', agregarCliente);
+  formularioGestionClientes.addEventListener('submit', function (event) {
+    event.preventDefault();
+    cliente.idC = identificacion.value;
+    cliente.nombreC = nombre.value;
+    cliente.apellidoC = apellido.value;
+    cliente.placaC = placa.value;
+    cliente.tipoC = tipo.value;
+    cliente.correoC = correo.value;
+    cliente.telefonoC = telefono.value;
 
-        listarClientes(gestionClientes);
-        formularioGestionClientes.reset();  //reset formulario//
-
-        formularioGestionClientes.addEventListener('submit',agregarCliente())
-    });
+    listarClientes(gestionClientes);
+    formularioGestionClientes.reset();
+    formularioGestionClientes.removeEventListener('submit', editarCliente);
+    formularioGestionClientes.addEventListener('submit', agregarCliente);
+  });
 }
 
 // eliminar cliente //
-function eliminarCliente(eliminar){
-    gestionClientes.splice(eliminar,1)
-    listarClientes(gestionClientes);
-};
+function eliminarCliente(index) {
+  gestionClientes.splice(index, 1);
+  listarClientes(gestionClientes);
+}
 
 //agregar al form//
 formularioGestionClientes.addEventListener('submit', agregarCliente);
 
 // funcion agregar cliente//
-function agregarCliente (event){
-    event.preventDefault();
-    const nuevoCliente = {};
-    nuevoCliente.idC = identificacion.value;
-    nuevoCliente.nombreC= nombre.value;
-    nuevoCliente.apellidoC = apellido.value;
-    nuevoCliente.placaC = placa.value;
-    nuevoCliente.tipoC = tipo.value;
-    nuevoCliente.correoC = correo.value;
-    nuevoCliente.telefonoC = telefono.value;
-    nuevoCliente.puntosC = 0;
+function agregarCliente(event) {
+  event.preventDefault();
 
-    gestionClientes.push(nuevoCliente);
-    listarClientes(gestionClientes);
-    formularioGestionClientes.reset();
+  const nuevoCliente = {};
+  nuevoCliente.idC = identificacion.value;
+  nuevoCliente.nombreC = nombre.value;
+  nuevoCliente.apellidoC = apellido.value;
+  nuevoCliente.placaC = placa.value;
+  nuevoCliente.tipoC = tipo.value;
+  nuevoCliente.correoC = correo.value;
+  nuevoCliente.telefonoC = telefono.value;
+  nuevoCliente.puntosC = 0;
+
+  gestionClientes.push(nuevoCliente);
+  listarClientes(gestionClientes);
+  formularioGestionClientes.reset();
 }
 
                                             // gestion servicios //
@@ -234,6 +230,7 @@ function listarServicios(servicios){
         });
 
     });
+    agregarCliente()
 }
 
 function eliminarServicio(index){
@@ -241,65 +238,63 @@ function eliminarServicio(index){
     listarServicios(gestionServicios)
 }
 
-// compra de servicios 
-
 const formularioCompras = document.getElementById('formularioCompras');
 const selectCliente = document.getElementById('selectCliente');
 const selectServicio = document.getElementById('selectServicio');
 
-formularioCompras.addEventListener('submit', function(event){
-    event.preventDefault()
-    let cedula = '';
-    let valorServicio = 0;
-    let puntos = 0;
+formularioCompras.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let cedula = '';
+  let valorServicio = 0;
+  let puntos = 0;
 
-    for(let servicio of gestionServicios){
-        if(servicio.vservicio == selectServicio.value){
-            valorServicio =parseFloat(servicio.Cvalor)
-            puntos = parseFloat(servicio.pts);
-        }
+  for (let servicio of gestionServicios) {
+    if (servicio.servicio == selectServicio.value) {
+      valorServicio = parseFloat(servicio.Cvalor);
+      puntos = parseFloat(servicio.pts);
     }
-    for(let cliente of gestionClientes){
-        if(cliente.nombreC == selectCliente.value){
-            cedula = cliente.idC;
-            cliente.puntosC += puntos;
-        }
-    }
-    alert (`        *Resumen de la compra*
+  }
 
-        identificacion:  ${cedula}
-        nombre cliente:  ${selectCliente.value}
-        nombre servicio: ${selectServicio.value}
-        valor Total:     ${valorServicio *0.26}
-        puntos ganados:  ${puntos}`)
-        totalPuntos(); 
+  for (let cliente of gestionClientes) {
+    if (cliente.nombreC == selectCliente.value) {
+      cedula = cliente.idC;
+      cliente.puntosC += puntos;
+    }
+  }
+
+  const valorSinDescuento = valorServicio / 1.14;
+  const valorTotal = valorServicio * 1.26;
+
+  alert(`        *Resumen de la compra*
+        Identificación: ${cedula}
+        Nombre cliente: ${selectCliente.value}
+        Nombre servicio: ${selectServicio.value}
+        Valor Total: $${valorTotal.toFixed(2)}
+        Puntos ganados: ${puntos}`);
+
+  totalPuntos();
 });
 
-function crearSelectorC (){
-    selectCliente.innerHTML = '<option selected>seleccionar cliente</option'
+function crearSelectorC() {
+  selectCliente.innerHTML = '<option selected>seleccionar cliente</option>';
 
-    for(let cliente of gestionClientes){
-        if(gestionClientes.length == 0){
-            return
-        }
+  for (let cliente of gestionClientes) {
     const crearOpcionCliente = document.createElement('option');
     crearOpcionCliente.textContent = cliente.nombreC;
     selectCliente.appendChild(crearOpcionCliente);
-} 
+  }
 }
 
-function crearSelectorS (){
-    selectServicio.innerHTML = '<option selected>seleccionar servicio</option>'
+function crearSelectorS() {
+  selectServicio.innerHTML = '<option selected>seleccionar servicio</option>';
 
-    for(let newServicio of gestionServicios ){
-        if(gestionServicios.length == 0){
-            return
-        }
-        const crearOpcionServicio = document.createElement('option');
-        crearOpcionServicio.textContent = newServicio.servicio;
-        selectServicio.appendChild(crearOpcionServicio);
-    }
+  for (let newServicio of gestionServicios) {
+    const crearOpcionServicio = document.createElement('option');
+    crearOpcionServicio.textContent = newServicio.servicio;
+    selectServicio.appendChild(crearOpcionServicio);
+  }
 }
+
 
 // puntos
 
